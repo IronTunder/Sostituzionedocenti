2d29c56a59b3c35243c4aaf931f1575245011efa
IronTunder <matteoone75@gmail.com>
2025-10-02 10:55:28 +0200

    02/10/25

--- src/LettoreCSV.java
+++ src/LettoreCSV.java
@@ -2,7 +2,6 @@ import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 import com.opencsv.exceptions.CsvException;
-
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.List;
