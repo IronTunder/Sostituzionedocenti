01533f979a76e4a4feeaa7eb5a49518a84217e98
mohamed.yaakoubi_isa <mohamed.yaakoubi@isarome.it>
2025-10-07 11:31:40 +0200

    07/10/25

--- src/Components/InterfacciaAggiornazione.java
+++ src/Components/InterfacciaAggiornazione.java
@@ -3,6 +3,9 @@ package Components;
 import javax.swing.*;
 
 public class InterfacciaAggiornazione extends JFrame {
+    public InterfacciaAggiornazione() {
+        setTitle("AGGIORNAMENTO FILE");
 
+    }
 
 }
